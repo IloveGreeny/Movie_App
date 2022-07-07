9b1ebf63acb121c6d1de7eda142c963c20cbe357
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-07-07 20:11:08 +0400

    Update Films.js

--- src/Components/Films.js
+++ src/Components/Films.js
@@ -76,7 +76,7 @@ export default function Films() {
     return(
         <div>
             <header>
-                <form id="form">
+                <form id="form" ref={form}>
                     <img className="image" width="100pxs"  src="https://static.miraheze.org/closinglogosgroupwiki/e/e1/GW266H196.jpeg.jpg?20211115093351" alt="logo"/>
                     <input
                         type="text"
